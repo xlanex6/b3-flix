828c7fe41efc026fa00c0ffc6fe5cea61340f256
xlanex6 <alexduval71@gmail.com>
2024-04-18 10:40:01 +0200

    basic fetch

---
+++ components/Movies/Card.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="">
+    <img :src="`https://image.tmdb.org/t/p/w300/${movie.poster_path}`" class=" h-56 w-auto" alt="">
+    <h3 class=" line-clamp-2">{{ movie.original_title }}</h3>
+    <p>{{ movie.vote_average }}</p>
+  </div>
+</template>
+
+<script setup>
+
+defineProps({
+  movie: Object
+})
+
+</script>
--- pages/movies/index.vue
+++ pages/movies/index.vue
@@ -1,14 +1,63 @@
 <template>
-MOVIES INDEX
-<p>
-  <NuxtLink to="/movies/123456">film 12346</NuxtLink>
-  <br>
-  <NuxtLink to="/movies/123457">film 12347</NuxtLink>
-  <br>
-  <NuxtLink to="/movies/123458">film 12348</NuxtLink>
-</p>
+  MOVIES INDEX
+
+
+  <div class="grid grid-flow-col gap-2">
+
+    <pre>{{ movies }}</pre>
+
+    <!-- <MoviesCard  :movie="movie" v-for="i in 12"/> -->
+  </div>
 </template>
 
 <script setup>
+
+const config = useRuntimeConfig()
+
+const movies = ref([]);
+
+// const movie = {
+//   "adult": false,
+//   "backdrop_path": "/xOMo8BRK7PfcJv9JCnx7s5hj0PX.jpg",
+//   "genre_ids": [
+//     878,
+//     12
+//   ],
+//   "id": 693134,
+//   "original_language": "en",
+//   "original_title": "Dune: Part Two",
+//   "overview": "Follow the mythic journey of Paul Atreides as he unites with Chani and the Fremen while on a path of revenge against the conspirators who destroyed his family. Facing a choice between the love of his life and the fate of the known universe, Paul endeavors to prevent a terrible future only he can foresee.",
+//   "popularity": 2938.734,
+//   "poster_path": "/1pdfLvkbY9ohJlCjQH2CZjjYVvJ.jpg",
+//   "release_date": "2024-02-27",
+//   "title": "Dune: Part Two",
+//   "video": false,
+//   "vote_average": 8.299,
+//   "vote_count": 2953
+// }
+// const url = 'https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1';
+// const options = {
+//   method: 'GET',
+//   headers: {
+//     accept: 'application/json',
+//     Authorization: 'Bearer '
+//   }
+// };
+
+const url = 'https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1';
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${config.public.tmdbKey}`
+  }
+};
+
+async function getNowPlayingMovies() {
+  const response = await $fetch(url, options);
+  movies.value = response.results;
+}
+
+getNowPlayingMovies()
 
 </script>
